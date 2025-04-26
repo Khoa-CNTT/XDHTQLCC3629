f08afcd15efe8cc095179ddd061687df2e272740
thiendev12345 <huuthien034501@gmail.com>
2025-04-26 11:48:22 +0700

    seeder

--- app/Models/NguyenLieu.php
+++ app/Models/NguyenLieu.php
@@ -10,10 +10,14 @@ class NguyenLieu extends Model
     use HasFactory;
     protected $table = 'nguyen_lieus';
     protected $fillable = [
+        "id_nha_san_xuat",
+        "id_nha_cung_cap",
         "ma_nguyen_lieu",
         "ten_nguyen_lieu",
-        "ma_lo_hang",
-        "ma_nha_cung_cap",
+        "so_luong",
+        "don_vi_tinh",
+        "ngay_san_xuat",
+        "han_su_dung",
         "tinh_trang"
     ];
 }
--- database/migrations/2025_02_25_101215_create_nguyen_lieus_table.php
+++ database/migrations/2025_02_25_101215_create_nguyen_lieus_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('nguyen_lieus', function (Blueprint $table) {
             $table->id();
+            $table->integer('id_nha_san_xuat')->nullable();
+            $table->integer('id_nha_cung_cap')->nullable();
             $table->string('ma_nguyen_lieu')->nullable();
             $table->string('ten_nguyen_lieu')->nullable();
-            $table->string('ma_lo_hang')->nullable();
-            $table->string('ma_nha_cung_cap')->nullable();
+            $table->integer('so_luong')->nullable();
+            $table->string('don_vi_tinh')->nullable();
+            $table->date('ngay_san_xuat')->nullable();
+            $table->date('han_su_dung')->nullable();
             $table->integer('tinh_trang')->default(1);
             $table->timestamps();
         });
